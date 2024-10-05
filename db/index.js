69e1f66cbbefbd4a006f3adabb544d1949f6a7e1
Anshuman Samanta <132367367+Anshuman22coder@users.noreply.github.com>
2024-10-05 23:31:19 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,17 +1,30 @@
-const mongoose=require("mongoose");
-mongoose.connect("mongodb+srv://admin:cQYKlyRCbZZX7PBp@anshuman.l4xvw.mongodb.net/NASA2");
+const mongoose = require("mongoose");
 
+// Single MongoDB connection
+const mongoURI = "mongodb+srv://admin:cQYKlyRCbZZX7PBp@anshuman.l4xvw.mongodb.net/NASA2";
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
 
-//definning schemas 
-const UserSchema=new mongoose.Schema({
-  //schema definition:::
-  username:String,
-  password:String,
-  age:Number
+// Defining User schema
+const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  age: Number,
+  teamName:String
+});
 
-})
-const User=mongoose.model('User',UserSchema);//modelling an db
-module.exports={
-  User
+// Defining Team schema with members as an array of usernames
+const TeamSchema = new mongoose.Schema({
+  teamName: String,
+  members: [String]  // Array to store multiple usernames
+});
+
+// Creating models for User and Team
+const User = mongoose.model('User', UserSchema);
+const Team = mongoose.model('Team', TeamSchema);
+
+module.exports = {
+  User,
+  Team
 };
-
